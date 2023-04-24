d0302d7cc75849c10e115bb9e48c61c72c669775
CaMi <jeanpaulcami@live.com>
2023-04-24 19:26:06 +0200

    start dashboard design

---
+++ resources/views/components/partials/_top_nav.blade.php
@@ -0,0 +1,71 @@
+<nav {{ $attributes->class(['z-10 w-full']) }} x-cloak x-data="{open:false}">
+    <div class="container px-10 py-3 mx-auto max-w-7xl">
+        <div class="flex justify-between lg:hidden">
+            <a class="text-xl font-bold text-white hover:text-primary" href="#">
+                <img src="{{ asset('img/hari-ubuzima-high-resolution-logo-color-on-transparent-background.png') }}"
+                     class="h-16" alt=""/>
+            </a>
+            <button class="text-white" @click="open=!open">
+                <svg x-show="!open" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                </svg>
+                <svg x-show="open" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                </svg>
+
+            </button>
+        </div>
+        <div x-show="open" style="opacity: 0.9;" id="navbarContent" x-transition
+             class="flex-col items-center justify-between transition duration-500 ease-in-out lg:flex lg:flex-row ">
+            <!-- Left Section -->
+            <div class="flex flex-col items-center gap-4 py-4 lg:flex-row">
+                <a class="text-sm font-semibold tracking-wide text-white uppercase hover:text-primary"
+                   href="{{ url('/')}}">
+                    Home
+                </a>
+                <a class="text-sm font-semibold tracking-wide text-gray-200 uppercase hover:text-primary"
+                   href="#menu">
+                    Menu
+                </a>
+                <a class="text-sm font-semibold tracking-wide text-gray-200 uppercase hover:text-primary"
+                   href="#reservation">
+                    Reservation
+                </a>
+            </div>
+
+            <!-- Middle Section -->
+            <div class="flex flex-col items-center justify-center flex-grow py-4 lg:flex-row">
+                <a class="text-xl font-bold text-white hover:text-primary" href="#">
+                    <img
+                            src="{{ asset('img/hari-ubuzima-high-resolution-logo-color-on-transparent-background.png') }}"
+                            class="h-16" alt=""/>
+                </a>
+            </div>
+
+            <!-- Right Section -->
+            <div class="flex flex-col items-center gap-4 py-4 lg:flex-row">
+                <a class="text-sm font-semibold tracking-wide text-gray-200 uppercase hover:text-primary"
+                   href="#events">
+                    News& Events
+                </a>
+                <a class="text-sm font-semibold tracking-wide text-gray-200 uppercase hover:text-primary"
+                   href="#gallery">
+                    Gallery
+                </a>
+                <a class="text-sm font-semibold tracking-wide text-gray-200 uppercase hover:text-primary"
+                   href="#contact">
+                    Contact
+                </a>
+                <a class="text-sm font-semibold tracking-wide text-gray-200 uppercase hover:text-primary"
+                   href="#about">
+                    About Us
+                </a>
+            </div>
+
+
+        </div>
+    </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,109 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+
+    <div class="flex flex-row" x-data="">
+        <div id="sidebar" class="w-80 bg-gray-100 min-h-screen p-4 border-r hidden" x-transition>
+            <div class="flex justify-between items-center mb-6">
+                <h2 class="text-lg font-semibold text-gray-900">Admin Panel</h2>
+
+            </div>
+            <div class="mt-8 flex items-center">
+                <div class="w-10 h-10 rounded-full bg-gray-400"></div>
+                <div class="ml-4">
+                    <div class="font-medium text-base text-gray-800">John Doe</div>
+                    <div class="mt-1">
+                        <a href="#" class="text-gray-600 hover:text-gray-900">Log out</a>
+                    </div>
+                </div>
+            </div>
+            <ul class="mt-8">
+                <li><a href="#" class="text-gray-700 hover:text-gray-900">Dashboard</a></li>
+                <li><a href="#" class="text-gray-700 hover:text-gray-900">Users</a></li>
+                <li><a href="#" class="text-gray-700 hover:text-gray-900">Settings</a></li>
+            </ul>
+        </div>
+        <div class="flex-1 p-4">
+            <button id="sidebar-toggle" class="text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900"
+                aria-label="Toggle sidebar">
+                <i class="fas fa-bars fa-2x text-primary"></i>
+            </button>
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
             @endif
-
+            {{-- <h2 class="text-lg font-semibold text-gray-900">Dashboard</h2>
+            <p class="mt-4">Welcome to the dashboard of the admin panel.</p>--}}
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
-    </body>
+    </div>
+
+    <script>
+        const toggleButton = document.getElementById('sidebar-toggle');
+        const sidebar = document.querySelector('#sidebar');
+        const icon = toggleButton.querySelector('i');
+        toggleButton.addEventListener('click', () => {
+            // sidebar.classList.toggle('hidden');
+            if (sidebar.classList.contains('hidden')) {
+                icon.classList.remove('fa-times');
+                icon.classList.add('fa-bars');
+                sidebar.classList.remove('hidden');
+                sidebar.classList.add('block');
+            } else {
+                icon.classList.remove('fa-bars');
+                icon.classList.add('fa-times');
+                sidebar.classList.remove('block');
+                sidebar.classList.add('hidden');
+            }
+        });
+
+        function toogleSideBarClass(){
+            if (window.innerWidth > 1024) {
+                sidebar.classList.remove('hidden');
+                sidebar.classList.add('block');
+                icon.classList.remove('fa-times');
+                icon.classList.add('fa-bars');
+            }
+            else{
+                sidebar.classList.remove('block');
+                sidebar.classList.add('hidden');
+                icon.classList.remove('fa-bars');
+                icon.classList.add('fa-times');
+            }
+        }
+        window.addEventListener('resize', function () {
+            toogleSideBarClass();
+        });
+        toogleSideBarClass();
+
+    </script>
+
+</body>
+
 </html>
